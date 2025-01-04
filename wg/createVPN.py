14568b1732d18571a8d6e1e56ae7bd25710003a7
lloydik <artemglazyrin@mail.ru>
2025-01-04 13:14:52 +0300

    Hotfix: 1

--- wg/createVPN.py
+++ wg/createVPN.py
@@ -62,7 +62,7 @@ class teamGenerator(object):
             env["client_num"] = client_num
             env["client_private_key"] = client[0]
             env["client_public_key"] = client[1]
-            tmp_ip = self.settings.ip_pool_base.format(cid=client_num + 2) if (client_num == -1) or not team_idx else self.settings.ip_pool_vulnbox.format(tid=team_idx,cid=2)
+            tmp_ip = self.settings.ip_pool_base.format(cid=client_num + 2) if (client_num != -1) or not team_idx else self.settings.ip_pool_vulnbox.format(tid=team_idx,cid=2)
             env["client_ip"] = tmp_ip + "/32"  # 0 and 1 reserved
             env["client_network"] = tmp_ip + "/24"  # todo: more networks?
             env["allowed_ips"] = env["client_ip"] + ',' + self.settings.ip_pool_vulnbox.format(tid=0,cid=0) + "/16"
@@ -84,7 +84,9 @@ class teamGenerator(object):
             f.write(self.settings.server_config_base.format(**env))
             f.write("\n\n" + "\n".join(client_parts))
 
+        # generate vulnbox server
         vulnbox_server = self.generate_key(self.epath, "server_vulnbox")
+        env['server_internal_addr'] = self.settings.ip_pool_vulnbox.format(tid=team_idx,cid=1) + "/24"
         env['server_private_key'] = vulnbox_server[0]
         env['server_public_key'] = vulnbox_server[1]
         env['port'] = self.settings.StartPort + 1000
